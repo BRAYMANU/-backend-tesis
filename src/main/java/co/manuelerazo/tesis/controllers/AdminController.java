37ae3bb5e04a2f80de4e216e411a7407161c3d07
Manuel Erazo <erazomanuelmanu@gmail.com>
2025-12-27 09:50:47 -0500

    subo admin controller

--- src/main/java/co/manuelerazo/tesis/controllers/AdminController.java
+++ src/main/java/co/manuelerazo/tesis/controllers/AdminController.java
@@ -1,5 +1,11 @@
 package co.manuelerazo.tesis.controllers;
 
+import java.util.List;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,8 +18,24 @@ public class AdminController {
     private final AdminService adminService;
 
     public AdminController(AdminService adminService){
-        this.adminService = adminService
+        this.adminService = adminService;
     }
+    
+    //1. Ver profesionales pendientes de validación
+    @GetMapping("/profesionales/pendientes")
+    public ResponseEntity<List<ProfesionalSaludResponseDTO>> obtenerProfesionalesPendientes(){
+        List<ProfesionalSaludResponseDTO> profesionalesPendientes = adminService.obtenerProfesionalesPendientes();
+        return ResponseEntity.ok(profesionalesPendientes);
+    }
+
+    //2. aprobar profesional en salud
+    @PutMapping("/profesionales/{id}/aprobar")
+    public ResponseEntity<ProfesionalSaludResponseDTO> aprobarProfesional(@PathVariable Integer id){
+        ProfesionalSaludResponseDTO profesionalAprobado = adminService.aprobarProfesional(id);
+        return ResponseEntity.ok(profesionalAprobado);
+    }
+
+
 
 
     
